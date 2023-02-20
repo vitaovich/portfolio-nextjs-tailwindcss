79a07cd2eabca471c4b9107c6895b30fafad325d
vitaovich <vitaliyalekhnovich@gmail.com>
2023-02-20 22:38:23 +0000

    update portfolio image

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 
+import selfImage from '../public/assets/images/vitaliy_alekhnovich.png'
+import selfImageMobile from '../public/assets/images/vitaliy_alekhnovich_mobile.png'
+
 const About = () => {
   const title = "About Me";
   const paragraphs = [
     {key: 1, text: "I'm Vitaliy Alekhnovich. I grew up in Kennewick, Washington after immigrating to the U.S. when I was three years old from a small village in Belarus. I've been fascinated with computers since I was little mainly because of the limitless possibilities they offer. I started coding my first few lines when I got my hands on a Parallax SumoBot kit in my freshman engineering class at Southridge High School. Ever since that day I've fallen in love with programming."},
     {key: 2, text: "I continue to hone my skills as a Software Engineer with patience and an insatiable thirst for knowledge. I love exploring all ideas and concepts whether that means working with assembly on MIPS to full stack web development in JavaScript, and even the pointer filled world of C. I don’t see challenges as road blocks. Instead, I see them as learning experiences to improve my skills."},
   ];
-  const selfImage = "/assets/images/vitaliy_alekhnovich.png";
   return (
     <section id="about">
       <div className="bg-orange-50 p-6 m-2 shadow-2xl rounded-md">
@@ -14,15 +16,18 @@ const About = () => {
           <h1 className="mb-2 text-black">
             {title}
           </h1>
-          <div className="flex flex-col justify-between lg:flex-row gap-x-5">
+          <div className="flex flex-col justify-between space-y-8 lg:flex-row gap-x-5 lg:space-y-0">
             <Image
               src={selfImage}
               alt="Image of Vitaliy"
-              className="rounded-md lg:rounded-l-md"
-              width={825}
-              height={638}
+              className="hidden lg:block rounded-l-md h-400"
             />
-            <div className="lg:w-[50%] space-y-5">
+            <Image
+              src={selfImageMobile}
+              alt="Image of Vitaliy"
+              className="lg:hidden rounded-md w-full"
+            />
+            <div className="space-y-5">
               {paragraphs.map((paragraph) => (
                 <p key={paragraph.key}>{paragraph.text}</p>
               ))}
