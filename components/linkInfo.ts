7ff8cdb0d07ab22d871a7d4fb85967ddef0fadf3
vitaovich <vitaliyalekhnovich@gmail.com>
2023-12-10 00:07:01 +0000

    add dev container and color buttons

--- components/linkInfo.ts
+++ components/linkInfo.ts
@@ -2,13 +2,15 @@ class LinkInfo {
   id: string;
   text: string;
   href: string;
+  classNames: string;
   background: boolean;
 
-  constructor(text: string, href: string, background: boolean) {
+  constructor(text: string, href: string, background: boolean, classNames:string = "") {
     this.text = text;
     this.id = new Date().toISOString();
     this.href = href;
     this.background = background;
+    this.classNames = classNames
   }
 }
 
