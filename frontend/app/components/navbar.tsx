a60150dacf9b90dcd4f79bb5bf5e2c15e2da6871
Vitaliy Alekhnovich <vitaliyalekhnovich@gmail.com>
2024-11-18 06:53:25 +0000

    move navbar and footer to layout

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from "react";
 import Image from 'next/image'
 import brandImg from '../../public/assets/images/brand.webp'
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,20 +1,16 @@
 "use client"
-import Footer from "./components/footer";
-import Navbar from "./components/navbar";
 import About from "./components/about";
 import Projects from "./components/projects";
 
 export default function Home() {
   return (
-    <div className="">
-    <Navbar></Navbar>
+    <>
     <div className="has-background-light">
       <div className={`lg:mx-32`}>
         <About></About>
         <Projects></Projects>
       </div>
     </div>
-    <Footer></Footer>
-  </div>
+  </>
   );
 }
